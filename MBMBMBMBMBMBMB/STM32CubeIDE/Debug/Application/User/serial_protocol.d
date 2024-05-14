014411daa3779364b805b6c4334e4d6e76fc4da6
AceOf5pade5 <creativenerf@gmail.com>
2024-05-14 12:06:08 +1000

    harlo
    asdf

---
+++ MBMBMBMBMBMBMB/STM32CubeIDE/Debug/Application/User/serial_protocol.d
@@ -0,0 +1,4 @@
+Application/User/serial_protocol.o: \
+ C:/Users/mattk/Downloads/X-CUBE-MEMS1-main/X-CUBE-MEMS1-main/Projects/NUCLEO-F401RE/Applications/CUSTOM/PresenceDetection_STHS34PF80/Src/serial_protocol.c \
+ ../../Inc/serial_protocol.h
+../../Inc/serial_protocol.h:
---
+++ MBMBMBMBMBMBMB/STM32CubeIDE/Debug/Drivers/BSP/Components/sths34pf80.d
@@ -0,0 +1,6 @@
+Drivers/BSP/Components/sths34pf80.o: \
+ C:/Users/mattk/Downloads/X-CUBE-MEMS1-main/X-CUBE-MEMS1-main/Drivers/BSP/Components/sths34pf80/sths34pf80.c \
+ C:/Users/mattk/Downloads/X-CUBE-MEMS1-main/X-CUBE-MEMS1-main/Drivers/BSP/Components/sths34pf80/sths34pf80.h \
+ C:/Users/mattk/Downloads/X-CUBE-MEMS1-main/X-CUBE-MEMS1-main/Drivers/BSP/Components/sths34pf80/sths34pf80_reg.h
+C:/Users/mattk/Downloads/X-CUBE-MEMS1-main/X-CUBE-MEMS1-main/Drivers/BSP/Components/sths34pf80/sths34pf80.h:
+C:/Users/mattk/Downloads/X-CUBE-MEMS1-main/X-CUBE-MEMS1-main/Drivers/BSP/Components/sths34pf80/sths34pf80_reg.h:
